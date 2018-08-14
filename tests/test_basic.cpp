700a6733fc87a6cf2f7acaa8424549b746bcb7cd
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-14 16:14:59 -0500

    added test for blank page

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,12 +1,21 @@
 #include "catch2/catch.hpp"
 #include "glue.h"
 #include "reader.h"
+#include "reader2.h"
 #include <iostream>
 
 
 TEST_CASE("dummy"){
-    Reader reader(TESS_DATA, "eng");
-    std::string d = reader.get_ocr(TEST_IMAGE);
+    Reader2 reader(TESS_DATA, "eng");
+    std::string d = reader.get_ocr(TEST_IMAGE_PATH "/" "engwithheadings.tif");
+    std::cout  << d << std::endl;
+    REQUIRE(!d.empty());
+}
+
+TEST_CASE("dummy2 blank page"){
+    Reader2 reader(TESS_DATA, "eng");
+//    std::string d = reader2.get_ocr(TEST_IMAGE);
+    std::string d = reader.get_ocr(TEST_IMAGE_PATH "/" "blankpage.tif");
     std::cout  << d << std::endl;
     REQUIRE(!d.empty());
 }
