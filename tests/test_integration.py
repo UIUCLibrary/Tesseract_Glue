e18b9ffad071f10dfa9808402a7b0fa5fe010157
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-14 16:17:56 -0500

    formatting

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,9 +6,7 @@ import ocr
 @pytest.mark.integration
 def test_reader(tessdata_eng, sample_images):
     reader = ocr.Reader(language_code="eng", tesseract_data_path=tessdata_eng)
-
-    test_image = os.path.join(
-        sample_images, "IlliniLore_1944_00000011.tif")
+    test_image = os.path.join(sample_images, "IlliniLore_1944_00000011.tif")
 
     text = reader.read(test_image)
     assert isinstance(text, str)
