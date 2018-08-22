d781bb38c4cfc637f710dc095364c32b715b4c83
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-22 15:34:45 -0500

    moved

--- ocr/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import tesseractwrap  # type: ignore
-from .reader import Reader
-from .engine import Engine
-__all__ = [
-    "Reader",
-    "Engine",
-    "tesseractwrap",
-]
--- ocr/utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "utils.h"
-#include <baseapi.h>
-
-std::string tesseract_version(){
-    return tesseract::TessBaseAPI::Version();
-}
