dc3d67aaa0f40b3354de6437bd50636cdb7d8e98
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-22 12:50:05 -0500

    remove old bindings

--- ocr/tesseractwrap.cpp
+++ ocr/tesseractwrap.cpp
@@ -8,8 +8,6 @@ PYBIND11_MODULE(tesseractwrap, m){
     options.enable_function_signatures();
     m.doc() = R"pbdoc(Wrapper to Tesseract's C++ API)pbdoc";
 
-    m.def("give_five", &give_five, "Gives you the number 5");
-    m.def("read_image", &read_image, "reads an image");
     m.def("set_tessdata", &set_tessdata, "sets the tessdata path");
     m.def("tesseract_version", &tesseract_version, "Get the version of tesseract being used");
 	pybind11::class_<Reader2>(m, "Reader").def(pybind11::init<const std::string &, const std::string &>()).def("get_ocr", &Reader2::get_ocr);
