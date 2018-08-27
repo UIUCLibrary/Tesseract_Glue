9ba21e29ad1b1bd6672814b8e76796a3615241d2
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-08-27 13:19:23 -0500

    added version test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,18 @@
+import re
+
 from uiucprescon import ocr
 import types
 
 
 def test_import_module():
     assert isinstance(ocr, types.ModuleType)
+
+
+def test_version():
+
+    e = ocr.Engine("")
+    version = e.get_version()
+    version_regex = re.compile("\d\.\d{2}\.\d{2}")
+    print(version)
+    assert version_regex.match(version)
 
