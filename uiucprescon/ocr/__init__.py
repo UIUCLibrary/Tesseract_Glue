d144734b3028376df2d9ffe7fa834f459b9219eb
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-02-20 15:25:55 -0600

    debug information about environment variable if importing tesseractwrap

--- uiucprescon/ocr/__init__.py
+++ uiucprescon/ocr/__init__.py
@@ -1,15 +1,26 @@
 import os as _os
+import sys as _sys
 
 _path = _os.path.abspath(_os.path.join(
     _os.path.dirname(__file__), 'tesseract', 'bin')) + ";" + _os.environ['PATH']
 _os.environ['PATH'] = _path
 
-from . import tesseractwrap
+try:
+    from . import tesseractwrap
+except ImportError as e:
+    print("Check path values", file=_sys.stderr)
+    for k, v in _os.environ.items():
+        print("{}:{}".format(k, v), file=_sys.stderr)
+    raise
+
+
 from .engine import Engine
 from .reader import Reader
+from .languages import LANGUAGE_CODES
 
 __all__ = [
     "Reader",
     "Engine",
     "tesseractwrap",
+    "LANGUAGE_CODES"
 ]
