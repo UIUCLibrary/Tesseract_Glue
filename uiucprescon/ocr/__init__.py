a269758e2527b638a9267f9aca2476c345fca666
Borchers, Henry Samuel <hborcher@illinois.edu>
2019-02-20 17:11:18 -0600

    debugging information for testing why can't import path

--- uiucprescon/ocr/__init__.py
+++ uiucprescon/ocr/__init__.py
@@ -1,16 +1,30 @@
 import os as _os
 import sys as _sys
+_paths = list(filter(lambda i: i.strip(), _os.environ['PATH'].split(";")))
+_tesseract_path = _os.path.abspath(_os.path.join(_os.path.dirname(__file__), 'tesseract', 'bin'))
+_new_paths = []
+for p in _paths:
+    if p == _tesseract_path:
+        break
+else:
+    _paths .insert(0, _tesseract_path)
+_path = ";".join(_paths)
 
-_path = _os.path.abspath(_os.path.join(
-    _os.path.dirname(__file__), 'tesseract', 'bin')) + ";" + _os.environ['PATH']
 _os.environ['PATH'] = _path
 
 try:
     from . import tesseractwrap
 except ImportError as e:
+    import subprocess
     print("Check path values", file=_sys.stderr)
     for k, v in _os.environ.items():
         print("{}:{}".format(k, v), file=_sys.stderr)
+
+    tesseract_exe = _os.path.join(_tesseract_path, "tesseract.exe")
+    if not _os.path.exists(tesseract_exe):
+        print("Did not find tesseract", file=_sys.stderr)
+    subprocess.call([tesseract_exe, "--version"])
+
     raise
 
 
