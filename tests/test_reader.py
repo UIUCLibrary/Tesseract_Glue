48b455b9c9431d2079352d763eabc45bda6a2063
henry <henryborchers@yahoo.com>
2021-02-23 08:46:54 -0600

    test_invalid_image_throws_error

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -13,3 +13,12 @@ def test_invalid_tesseract_data_path():
         )
 
 
+def test_invalid_image_throws_error(tessdata_eng):
+
+    with pytest.raises(RuntimeError):
+
+        reader = ocr.Reader(
+            language_code="eng",
+            tesseract_data_path=tessdata_eng
+        )
+        reader.read_image(None)
