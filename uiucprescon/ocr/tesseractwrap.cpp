9cd0ee1e021d221809ca7c488d0de74bab372bb5
henry <henryborchers@yahoo.com>
2021-02-22 21:32:27 -0600

    white space

--- uiucprescon/ocr/tesseractwrap.cpp
+++ uiucprescon/ocr/tesseractwrap.cpp
@@ -9,16 +9,19 @@ PYBIND11_MODULE(tesseractwrap, m){
     options.enable_function_signatures();
     m.doc() = R"pbdoc(Wrapper to Tesseract's C++ API)pbdoc";
 
-//    m.def("set_tessdata", &set_tessdata, "sets the tessdata path");
     m.def("tesseract_version", &tesseract_version, "Get the version of tesseract being used");
+
     pybind11::class_<Pix, std::shared_ptr<Pix>>(m, "Pix")
             .def(pybind11::init<>());
+
     pybind11::class_<Image, std::shared_ptr<Image>>(m, "Image")
             .def_property_readonly("w", &Image::get_w)
             .def_property_readonly("h", &Image::get_h)
             ;
+
     m.def("load_image", &load_image, "Load image file");
-	pybind11::class_<Reader2>(m, "Reader")
+
+    pybind11::class_<Reader2>(m, "Reader")
 	        .def(pybind11::init<const std::string &, const std::string &>())
 	        .def("get_ocr", &Reader2::get_ocr_from_image)
 	        ;
