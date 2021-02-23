6d461b62f6156d18b0f1c025aa243236a6e2f0e7
henry <henryborchers@yahoo.com>
2021-02-23 08:47:12 -0600

    whitespace

--- tests/test_load_image.py
+++ tests/test_load_image.py
@@ -4,4 +4,4 @@ import os
 def test_load_image(sample_images):
     sample_image = os.path.join(sample_images, "IlliniLore_1944_00000011.tif")
     image = ocr.load_image(sample_image)
-    assert isinstance(image.w, int) and  isinstance(image.h, int)
+    assert isinstance(image.w, int) and isinstance(image.h, int)
