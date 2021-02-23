66778bdf1da3655c8176119b75c446e8f1b0eae8
henry <henryborchers@yahoo.com>
2021-02-23 09:04:53 -0600

    test_valid_image

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from uiucprescon import ocr
 
@@ -22,3 +23,13 @@ def test_invalid_image_throws_error(tessdata_eng):
             tesseract_data_path=tessdata_eng
         )
         reader.read_image(None)
+
+def test_valid_image(tessdata_eng, sample_images):
+
+    sample_image = os.path.join(sample_images, "IlliniLore_1944_00000011.tif")
+    reader = ocr.Reader(
+        language_code="eng",
+        tesseract_data_path=tessdata_eng
+    )
+    image = reader.read_image(sample_image)
+    assert image is not None
