6e8ffddaa039d5d7b92e8b9742a88898973f8ac4
henry <henryborchers@yahoo.com>
2021-02-25 20:21:07 -0600

    removed commented out code

--- uiucprescon/ocr/Capabilities.cpp
+++ uiucprescon/ocr/Capabilities.cpp
@@ -8,20 +8,3 @@ std::string Capabilities::ImagelibVersions() {
     std::unique_ptr<char> ptr(getImagelibVersions(), std::default_delete<char>());
     return std::string(ptr.get());
 }
-//
-//std::map<const std::string, std::string> Capabilities::ImagelibVersions2() {
-//    std::map<const std::string, std::string> data;
-//
-//    #define STRINGIFY(x) #x
-//    #define TOSTRING(x) STRINGIFY(x)
-//
-//    #ifdef LIBJP2K_HEADER
-//    data["jp2k"] = TOSTRING(LIBJP2K_HEADER);
-////    #include LIBJP2K_HEADER
-////    char *version = opj_version();
-//    #endif
-//
-//    #undef STRINGIFY
-//    #undef TOSTRING
-//    return data;
-//}
