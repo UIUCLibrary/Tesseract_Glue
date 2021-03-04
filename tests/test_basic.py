d6907e4092a0c5f335080d92ee3bcd69a8f07800
henry <henryborchers@yahoo.com>
2021-03-04 16:14:51 -0600

    record_property

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,29 +2,30 @@ import re
 
 from uiucprescon import ocr
 import types
-
+import pytest
 
 def test_import_module():
     assert isinstance(ocr, types.ModuleType)
 
 
-def test_version():
+def test_version(record_property):
 
     e = ocr.Engine("")
     version = e.get_version()
     version_regex = re.compile("[0-9][.][0-9]{1,2}[.][0-9]{1,2}")
-    print(version)
+    record_property("version", version)
     assert version_regex.match(version)
 
 
-def test_get_image_lib_versions():
+def test_get_image_lib_versions(record_property):
     data = ocr.tesseractwrap.get_image_lib_versions()
-    print(data)
+    record_property("lib_versions", data)
     assert isinstance(data, str)
 
 
 
-def test_get_tesseract_version():
+def test_get_tesseract_version(record_property):
     data = ocr.tesseractwrap.tesseract_version()
+    record_property("tesseract_version", data)
     assert isinstance(data, str)
 
