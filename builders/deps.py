2e2530b414bb7eec0ed0638a9587870e10770d37
henry <henryborchers@yahoo.com>
2021-03-05 18:44:59 -0600

    instsall pybind11 from pypi

--- builders/deps.py
+++ builders/deps.py
@@ -4,6 +4,7 @@ from typing import List
 DEPS_REGEX = \
     r'(?<=(Image has the following dependencies:(\n){2}))((?<=\s).*\.dll\n)*'
 
+
 def parse_dumpbin_deps(file) -> List[str]:
 
     dlls = []
@@ -17,3 +18,18 @@ def parse_dumpbin_deps(file) -> List[str]:
             dll = x.strip()
             dlls.append(dll)
     return dlls
+
+
+def remove_system_dlls(dlls):
+    non_system_dlls = []
+    for dll in dlls:
+        if dll.startswith("api-ms-win-crt"):
+            continue
+
+        if dll.startswith("python"):
+            continue
+
+        if dll == "KERNEL32.dll":
+            continue
+        non_system_dlls.append(dll)
+    return non_system_dlls
