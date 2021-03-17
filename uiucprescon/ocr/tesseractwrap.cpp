579350412f39bb9e35d1d06df7818f69a56ed881
Henry Borchers <hborcher@illinois.edu>
2021-03-17 15:26:07 -0500

    tesseractwrap extension inits the classes before the functions to avoid the already registered error (#67)
    * pybind11 is a requirement of build back end
    
    * define classes before functions
    
    * init order
    
    * pybind11>=2.5 removed from setup_requires
    
    * pybind11>=2.5 is back in pyproject.toml

--- uiucprescon/ocr/tesseractwrap.cpp
+++ uiucprescon/ocr/tesseractwrap.cpp
@@ -13,24 +13,26 @@ PYBIND11_MODULE(tesseractwrap, m){
     options.enable_function_signatures();
     m.doc() = R"pbdoc(Wrapper to Tesseract's C++ API)pbdoc";
 
+    // the classes need to before the  functions otherwise you get an generic_type is already registered error
+    pybind11::class_<Image, std::shared_ptr<Image>>(m, "Image", pybind11::module_local())
+            .def_property_readonly("w", &Image::get_w)
+            .def_property_readonly("h", &Image::get_h);
+
+    pybind11::class_<Pix, std::shared_ptr<Pix>>(m, "Pix", pybind11::module_local())
+            .def(pybind11::init<>());
+
+    pybind11::class_<Reader2>(m, "Reader")
+            .def(pybind11::init<const std::string &, const std::string &>())
+            .def("get_ocr", &Reader2::get_ocr_from_image);
+
+    // ================================================================================================================
     m.def("tesseract_version", &tesseract_version, "Get the version of tesseract being used");
     m.def("get_image_lib_versions", [](){
         return Capabilities::ImagelibVersions();
         }, "Get the version of image libraries being used");
 
-    pybind11::class_<Pix, std::shared_ptr<Pix>>(m, "Pix")
-            .def(pybind11::init<>());
-
-    pybind11::class_<Image, std::shared_ptr<Image>>(m, "Image")
-            .def_property_readonly("w", &Image::get_w)
-            .def_property_readonly("h", &Image::get_h)
-            ;
-
     m.def("load_image", &load_image, "Load image file");
     pybind11::register_exception<TesseractGlueException>(m, "TesseractGlueException", PyExc_RuntimeError);
 
-    pybind11::class_<Reader2>(m, "Reader")
-	        .def(pybind11::init<const std::string &, const std::string &>())
-	        .def("get_ocr", &Reader2::get_ocr_from_image)
-	        ;
+
 }
