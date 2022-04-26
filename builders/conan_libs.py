fe0e4cbfd834ca622eb06e15e58f1ea049b826be
henry <henryborchers@yahoo.com>
2022-04-26 10:39:01 -0500

    msvc runtime=MD

--- builders/conan_libs.py
+++ builders/conan_libs.py
@@ -286,6 +286,8 @@ class BuildConan(setuptools.Command):
         settings.append(f"compiler.version={compiler_info['version']}")
         if get_compiler_name() == "msvc":
             settings.append(f"compiler.cppstd=14")
+            settings.append(f"compiler.runtime=MD")
+
         conanfile_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__), "..")
         )
