6ce008822191472f98455b92b87a8713c2ab9222
henry <henryborchers@yahoo.com>
2022-10-28 14:20:28 -0500

    linux only shared lib iconv

--- conanfile.py
+++ conanfile.py
@@ -15,8 +15,13 @@ class Exiv2BindConan(ConanFile):
         self.copy("*.dll", dst=".", src="bin")  # From bin to bin
         self.copy("libtiff.so.*", dst="lib", src="lib")  # From bin to bin
         self.copy("libtiff.so", dst="lib", src="lib")  # From bin to bin
+        self.copy("libiconv.so", dst="lib", src="lib")  # From bin to bin
+        self.copy("libiconv.so.*", dst="lib", src="lib")  # From bin to bin
+        self.copy("libcharset.so", dst="lib", src="lib")  # From bin to bin
+        self.copy("libcharset.so.*", dst="lib", src="lib")  # From bin to bin
         self.copy("tesseract", dst="", src="bin", keep_path=True)  # From bin to bin
 
     def configure(self):
         if self.settings.os == "Linux":
             self.options["libtiff"].shared = True
+            self.options["libiconv"].shared = True
