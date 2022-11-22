fe04a49d74bacad7f946075062dae3f6d1b6a429
henry <henryborchers@yahoo.com>
2022-11-22 14:11:53 -0600

    config_settings

--- builders/local_backend.py
+++ builders/local_backend.py
@@ -16,6 +16,7 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
     if config_settings is not None and config_settings.get('conan_cache') is not None:
         if "CONAN_USER_HOME" in os.environ:
             config_settings['conan_cache'] = os.path.join(os.environ["CONAN_USER_HOME"], ".conan")
+
     conan_libs.build_conan(
         wheel_directory,
         config_settings,
@@ -23,9 +24,11 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
         install_libs=False
     )
     original_conan_user_home = os.getenv("CONAN_USER_HOME")
+
     try:
         if config_settings is not None and "conan_cache" in config_settings:
-            os.environ["CONAN_USER_HOME"] = os.path.normpath(os.path.join(config_settings['conan_cache'], ".."))
+            os.environ["CONAN_USER_HOME"] = config_settings['conan_cache']
+            # os.environ["CONAN_USER_HOME"] = os.path.normpath(os.path.join(config_settings['conan_cache'], ".."))
         return setuptools.build_meta.build_wheel(wheel_directory, config_settings, metadata_directory)
     finally:
         if original_conan_user_home:
