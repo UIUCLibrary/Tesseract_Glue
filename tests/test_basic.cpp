4c1f44ae54262e9b5560ad3be5cd27454e49195a
Henry Borchers <henryborchers@yahoo.com>
2025-06-10 11:45:22 -0500

    test: Image size tests

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -57,6 +57,30 @@ TEST_CASE("Image"){
         }
 
     }
+}
+TEST_CASE("Image size"){
+    const std::vector<std::tuple<std::string, int, int>> test_cases = {
+        {"blankpage.tif",       3000,   2234    },
+        {"engwithheadings.tif", 3000,   1969    },
+        {"engwithpicture.tif",  3000,   1982    },
+        {"ita.tif",             3000,   1826    },
+        {"productionnotes.tif", 3000,   2065    },
+    };
+    for (const auto& tuple : test_cases) {
+        DYNAMIC_SECTION("checking " << std::get<0>(tuple)) {
+            const std::string image_path = TEST_IMAGE_PATH;
+
+            std::shared_ptr<Image> image = load_image(image_path + "/" + std::get<0>(tuple));
+            const auto height = std::get<1>(tuple);
+            DYNAMIC_SECTION("image has height of " << height){
+                REQUIRE(image->get_h() == height);
+            }
+            const auto width = std::get<2>(tuple);
+            DYNAMIC_SECTION("image has width of " << width){
+                REQUIRE(image->get_w() == width);
+            }
+        }
+}
 
 }
 TEST_CASE("ImageLoader"){
