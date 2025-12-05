b5c761f04e88fa26e1cfd2e5e4e4c92621fc13c2
Henry Borchers <henryborchers@yahoo.com>
2025-12-05 16:20:10 -0600

    fix coverage

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -611,9 +611,8 @@ def call(){
                                                             script: '''mkdir -p reports/pytestcoverage
                                                                        uv run coverage run --parallel-mode --source=src -m pytest --junitxml=./reports/pytest/junit-pytest.xml --basetemp=/tmp/pytest
                                                                        ls -laR build/temp
-                                                                       uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep build/
+                                                                       uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/
                                                                     '''
-//                                                                        uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/
 //                                                                        uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/temp
                                                         )
                                                     }
@@ -670,9 +669,10 @@ def call(){
                                                         script: 'build/cpp/tests/tester -r sonarqube -o reports/test-cpp.xml'
                                                     )
                                                     sh '''mkdir -p reports/coverage
-                                                          uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --keep -print-summary --json=$WORKSPACE/reports/coverage/coverage_cpp_tests.json --txt=$WORKSPACE/reports/coverage/text_cpp_tests_summary.txt --gcov-object-directory=$WORKSPACE/cpp_tests_coverage_data build/cpp
+                                                          uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --keep -print-summary --json=$WORKSPACE/reports/coverage/coverage_cpp_tests.json --txt=$WORKSPACE/reports/coverage/text_cpp_tests_summary.txt build/cpp
                                                           cat reports/coverage/text_cpp_tests_summary.txt
                                                        '''
+//                                                           uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --keep -print-summary --json=$WORKSPACE/reports/coverage/coverage_cpp_tests.json --txt=$WORKSPACE/reports/coverage/text_cpp_tests_summary.txt --gcov-object-directory=$WORKSPACE/cpp_tests_coverage_data build/cpp
                                                 }
                                                 post{
                                                     always{
