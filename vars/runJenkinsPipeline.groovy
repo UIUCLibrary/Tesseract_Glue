0f3c058f752939a2749c26d2713745b2eabda774
Henry Borchers <henryborchers@yahoo.com>
2025-12-05 16:19:19 -0600

    fix coverage

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -611,8 +611,9 @@ def call(){
                                                             script: '''mkdir -p reports/pytestcoverage
                                                                        uv run coverage run --parallel-mode --source=src -m pytest --junitxml=./reports/pytest/junit-pytest.xml --basetemp=/tmp/pytest
                                                                        ls -laR build/temp
-                                                                       uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/
+                                                                       uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep build/
                                                                     '''
+//                                                                        uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/
 //                                                                        uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --exclude-directories build/python/temp/conan_cache --print-summary --keep --gcov-object-directory=$WORKSPACE/cpp_extension_tests_coverage_data build/temp
                                                         )
                                                     }
