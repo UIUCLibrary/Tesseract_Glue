9c86f48feb8df82b400c50483713dba82d817cc1
Henry Borchers <henryborchers@yahoo.com>
2025-12-09 14:18:36 -0600

    ci: fix coverage

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -542,7 +542,7 @@ def call(){
                             }
                             stage('Building Documentation'){
                                 environment{
-                                    GCOV_PREFIX="\$WORKSPACE/build/temp"
+                                    GCOV_PREFIX="${env.WORKSPACE}/build/temp"
                                     GCOV_PREFIX_STRIP=0
                                 }
                                 steps{
@@ -579,7 +579,7 @@ def call(){
                                         stages{
                                             stage('Python Tests'){
                                                 environment{
-                                                    GCOV_PREFIX="\$WORKSPACE/build/temp"
+                                                    GCOV_PREFIX="${env.WORKSPACE}/build/temp"
                                                     GCOV_PREFIX_STRIP=0
                                                 }
                                                 steps{
