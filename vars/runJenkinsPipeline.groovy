f01e76ecb5fe2e8118fc1dc7406405af2cb2bb4b
Henry Borchers <henryborchers@yahoo.com>
2025-12-09 10:50:53 -0600

    ci: fix coverage

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -653,6 +653,9 @@ def call(){
                                                                 sh(label: 'Creating gcovr coverage report',
                                                                    script: 'uv run gcovr --root $WORKSPACE --filter=src/uiucprescon/ocr --keep --exclude-directories $WORKSPACE/build/cpp --exclude-directories $WORKSPACE/build/python/temp/conan_cache --print-summary --json=$WORKSPACE/reports/coverage/coverage-c-extension_tests.json --txt=$WORKSPACE/reports/coverage/coverage-c-extension_tests.txt --exclude-throw-branches --fail-under-line=1 --gcov-object-directory=$WORKSPACE/build/temp --verbose build/temp'
                                                                 )
+                                                            } catch (e){
+                                                                sh 'ls -laR'
+                                                                throw e
                                                             } finally {
                                                                 sh 'cat reports/coverage/coverage-c-extension_tests.txt'
                                                             }
