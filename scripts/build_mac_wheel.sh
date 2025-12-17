b32bce1c4565b6152e275068ee3b4deae22e22da
Henry Borchers <henryborchers@yahoo.com>
2025-12-17 11:51:18 -0600

    fix: build_mac_wheel.sh now explicitly make non-freethreaded versions if the version number does not include a "t" at end

--- scripts/build_mac_wheel.sh
+++ scripts/build_mac_wheel.sh
@@ -52,6 +52,7 @@ generate_wheel(){
     out_temp_wheels_dir=$(mktemp -d /tmp/python_wheels.XXXXXX)
     output_path="./dist"
     trap 'rm -rf $out_temp_wheels_dir' ERR SIGINT SIGTERM RETURN
+    echo "Building wheel for Python $python_version on macOS $processor_type"
     _PYTHON_HOST_PLATFORM=$_PYTHON_HOST_PLATFORM MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET ARCHFLAGS=$ARCHFLAGS $uv_exec build --wheel --out-dir=$out_temp_wheels_dir --python=$python_version $project_root
     pattern="$out_temp_wheels_dir/*.whl"
     files=( "$pattern" )
