c41d7673e9b02b080abf6e306933e7f6ee54d697
hborcher <hborcher@illinois.edu>
2026-08-03 14:57:12 -0500

    feat: no longer need manipulate path during import
    No longer needed because using all static libraries

--- src/uiucprescon/ocr/__init__.py
+++ src/uiucprescon/ocr/__init__.py
@@ -1,30 +1,15 @@
 """Optical character recognition that Use Google Tesseract."""
-import os as _os
 
-# the path to the linking shared libraries need to added to the path env var
-# to properly work
-_paths = list(filter(lambda i: i.strip(), _os.environ['PATH'].split(";")))
-
-_tesseract_path = _os.path.abspath(
-    _os.path.join(_os.path.dirname(__file__), 'tesseract', 'bin'))
-
-for p in _paths:
-    if p == _tesseract_path:
-        break
-else:
-    _paths.insert(0, _tesseract_path)
-
-# _os.environ['PATH'] = ";".join(_paths)
-# pylint: disable=wrong-import-position
-from . import tesseractwrap                   # noqa: E402
-from .tesseractwrap import load_image         # noqa: E402
-from .tesseractwrap import PDFBuilder         # noqa: E402
-from .tesseractwrap import OCRApi             # noqa: E402
-from .engine import Engine                    # noqa: E402
-from .reader import Reader                    # noqa: E402
-from .languages import LANGUAGE_CODES         # noqa: E402
-from .capabilities import image_lib_versions  # noqa: E402
 from . import utils
+from . import tesseractwrap
+from .tesseractwrap import load_image
+from .tesseractwrap import PDFBuilder
+from .tesseractwrap import OCRApi
+from .engine import Engine
+from .reader import Reader
+from .languages import LANGUAGE_CODES
+from .capabilities import image_lib_versions
+
 __all__ = [
     "Reader",
     "Engine",
