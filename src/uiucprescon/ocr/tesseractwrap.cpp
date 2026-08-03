15773d8c61abb146c0dcc3d0cf681c31a4fbca2d
hborcher <hborcher@illinois.edu>
2026-08-03 14:01:02 -0500

    refactor: optimize header imports

--- src/uiucprescon/ocr/tesseractwrap.cpp
+++ src/uiucprescon/ocr/tesseractwrap.cpp
@@ -6,41 +6,42 @@
 #include "reader2.h"
 #include "utils.h"
 
-#include <iostream>
 #include <memory>
 #include <string>
 
-#include <leptonica/alltypes.h>
+#include <pybind11/cast.h>
+#include <pybind11/detail/common.h>
+#include <pybind11/options.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <pybind11/pytypes.h>
 
 namespace py = pybind11;
 namespace ocr = uiucprescon::ocr;
 namespace glue = uiucprescon::glue;
 
-PYBIND11_MODULE(tesseractwrap, m, py::mod_gil_not_used()) {
+PYBIND11_MODULE(tesseractwrap, mod, py::mod_gil_not_used()) {
     py::options options;
-    py::register_exception<glue::TesseractGlueException>(m, "TesseractGlueException");
+    py::register_exception<glue::TesseractGlueException>(mod, "TesseractGlueException");
     options.enable_function_signatures();
-    m.doc() = R"pbdoc(Wrapper to Tesseract's C++ API)pbdoc";
+    mod.doc() = R"pbdoc(Wrapper to Tesseract's C++ API)pbdoc";
 
     // the classes need to be declared before the functions, otherwise you get a generic_type is already registered
     // error
-    py::class_<ocr::Image, std::shared_ptr<ocr::Image>>(m, "Image", py::module_local())
+    py::class_<ocr::Image, std::shared_ptr<ocr::Image>>(mod, "Image", py::module_local())
         .def_property_readonly("w", &ocr::Image::get_w)
         .def_property_readonly("h", &ocr::Image::get_h);
-    py::class_<ocr::OCRApi, std::shared_ptr<ocr::OCRApi>>(m, "OCRApi", "OCR Api for tesseract API.")
+    py::class_<ocr::OCRApi, std::shared_ptr<ocr::OCRApi>>(mod, "OCRApi", "OCR Api for tesseract API.")
         .def(py::init(&ocr::OCRApi::create), py::arg("datapath"), py::arg("language_code"))
         .def_property_readonly("datapath", &ocr::OCRApi::get_tesseract_data_path);
 
-    py::class_<ocr::Reader2>(m, "Reader")
+    py::class_<ocr::Reader2>(mod, "Reader")
         .def(py::init([](const std::string& tessdata, const std::string& language_code) {
                  const auto api = ocr::OCRApi::create(tessdata, language_code);
                  return ocr::Reader2(api);
              }),
              py::arg("tessdata"), py::arg("language_code"))
         .def("get_ocr", &ocr::Reader2::get_ocr_from_image);
-    py::class_<ocr::PDFBuilder>(m, "PDFBuilder", "PDFBuilder builder class.")
+    py::class_<ocr::PDFBuilder>(mod, "PDFBuilder", "PDFBuilder builder class.")
         .def(py::init([](const std::string& filename, const std::shared_ptr<ocr::OCRApi>& api,
                          const std::string& title) { return ocr::PDFBuilder(filename, api, title); }),
              py::arg("filename"), py::arg("api"), py::arg("title") = std::string(""))
@@ -68,11 +69,11 @@ PYBIND11_MODULE(tesseractwrap, m, py::mod_gil_not_used()) {
             },
             py::return_value_policy::reference);
     // ================================================================================================================
-    m.def("tesseract_version", &ocr::tesseract_version, "Get the version of tesseract being used");
-    m.def("get_image_lib_versions", &ocr::Capabilities::ImagelibVersions,
-          "Get the version of image libraries being used");
-    m.def("load_image", &glue::load_image, "Load image file");
-    m.def(
+    mod.def("tesseract_version", &ocr::tesseract_version, "Get the version of tesseract being used");
+    mod.def("get_image_lib_versions", &ocr::Capabilities::ImagelibVersions,
+            "Get the version of image libraries being used");
+    mod.def("load_image", &glue::load_image, "Load image file");
+    mod.def(
         "pixScaleToSize",
         [](const ocr::Image& image, int targetWidth, int targetHeight) {
             return glue::pixScaleToSize(image, targetWidth, targetHeight);
